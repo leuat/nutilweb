40a4c9f3c3d1bfb5acd41896fcba3a24a7b72676
Leuat <leuat@irio.co.uk>
2020-10-30 11:22:47 +0100

    back to linux

--- executeNutil.sh
+++ executeNutil.sh
@@ -4,7 +4,8 @@ mkdir work
 cp temp.zip work
 cd work
 unzip temp.zip
-/home/leuat/code/nutil/Debug/Nutil nutil.nut 4
+#/home/leuat/code/nutil/Debug/Nutil nutil.nut 4
+open /Users/leuat/code/nutil/Debug/Nutil.app nutil.nut 4
 zip -r results.zip 4_output_dir
 mv results.zip ../../results/results_$1.zip
 cd ..
